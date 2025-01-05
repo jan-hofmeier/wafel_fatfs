5705feb59a60eff4f059cd02b31465ce91b7c55a
Jan Hofmeier <jan.hofmeier@gmail.com>
2025-01-05 02:40:55 +0100

    sync

--- source/salio.c
+++ source/salio.c
@@ -13,11 +13,14 @@ static const char *MODULE_NAME = "SALIO";
 
 
 
-uint32_t (*FSSAL_RawRead)(FSSALHandle device,uint32_t lba_hi,uint lba, uint32_t blkCount,void *buf,
+uint32_t (*FSSAL_RawRead)(FSSALHandle device,uint32_t lba_hi,uint lba, uint32_t blkCount, void *buf,
                       void (*cb)(int, void*),void *cb_ctx) = (void*)0x10732bc0;
 
-uint32_t (*FSSAL_RawWrite)(FSSALHandle device,uint32_t lba_hi,uint lba, uint32_t blkCount,void *buf,
+uint32_t (*FSSAL_RawWrite)(FSSALHandle device,uint32_t lba_hi,uint lba, uint32_t blkCount, void *buf,
                       void (*cb)(int, void*),void *cb_ctx) = (void*)0x0107328a0;
+
+uint32_t (*FSSAL_Sync)(FSSALHandle device,uint32_t lba_hi,uint lba, uint32_t blkCount,
+                      void (*cb)(int, void*),void *cb_ctx) = (void*)0x10732314;
 
 FSSALDevice* (*FSSAL_LookupDevice)(FSSALHandle device) = (void*)0x10733990;
 
@@ -53,20 +56,9 @@ static void rw_callback(int res, void *ctx){
 
 static DRESULT aligned_sync_rw(BYTE pdrv, BYTE* buff, LBA_t sector, UINT count, uint32_t (*raw_rw)(FSSALHandle device,uint32_t lba_hi,uint lba, uint32_t blkCount,void *buf,
                       void (*cb)(int, void*),void *cb_ctx)){
-    rw_cb_ctx ctx = {iosCreateSemaphore(1,0)};
-    if(ctx.semaphore < 0){
-        debug_printf("%s: Error creating Semaphore: 0x%X\n", MODULE_NAME, ctx.semaphore);
-        return RES_NOTRDY;
-    }
-    //int res = raw_rw(/* fatfs_volumes[pdrv].handle */ *((int*)0x10bb7e4c), sector>>32, sector, count, buff, rw_callback, &ctx);
-    int res = raw_rw(device_handles[pdrv], sector>>32, sector, count, buff, rw_callback, &ctx);
+
+    int res = raw_rw(device_handles[pdrv], sector>>32, sector, count, buff, NULL, NULL);
     debug_printf("%s: raw_rw returned: %x\n", MODULE_NAME, res);
-    if(!res){
-        iosWaitSemaphore(ctx.semaphore, 0);
-        res = ctx.res;
-    }
-    iosDestroySemaphore(ctx.semaphore);
-    debug_printf("%s: sync_rw res: %x\n", MODULE_NAME, res);
     return res?RES_ERROR:RES_OK;
 }
 
@@ -113,6 +105,10 @@ DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff){
 
     switch (cmd)
     {
+        case CTRL_SYNC:
+            int res = FSSAL_Sync(device_handles[pdrv], 0, 0, 0, NULL, NULL);
+            debug_printf("SYNC res: 0x%x\n", res);
+            return res?RES_ERROR:RES_OK;
         case GET_SECTOR_SIZE:
             debug_printf("SECTOR SIZE: %i\n", sector_sizes[pdrv]);
             *(WORD*)buff = sector_sizes[pdrv];
@@ -125,7 +121,7 @@ DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff){
             bc <<= 32;
             bc |= device->block_count;
             *(LBA_t*)buff = bc;
-            return RES_OK; 
+            return RES_OK;
     
     }
     debug_printf("UNKOWN IOCTL %i\n", cmd);
